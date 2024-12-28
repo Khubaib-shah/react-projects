e37aa93d3c6cf0e3160c50a8bb64157aa6ffde6b
khubaib shah <bewithbaacha@gmail.com>
2024-12-28 07:31:37 +0500

    used cartContext

--- shopping-cart/src/App.jsx
+++ shopping-cart/src/App.jsx
@@ -1,13 +1,18 @@
-import { allProducts } from "./assets/data";
+import { useEffect } from "react";
 import CartItem from "./components/CartItem";
+import { useCart } from "./context/cartContext";
 function App() {
+  const { allItems, setItems } = useCart();
+  useEffect(() => {
+    setItems();
+  }, []);
   return (
     <div className="grid place-items-center py-20">
       <h1 className="text-5xl italic text-gray-500 mb-16 ">
         Trend Alert: Must-Have Outfits of the session
       </h1>
       <div className="grid grid-cols-3 place-content-start gap-10">
-        {allProducts?.map((item) => {
+        {allItems?.map((item) => {
           return <CartItem key={item.id} item={item} />;
         })}
       </div>
